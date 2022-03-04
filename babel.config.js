adce7001f70992073c7830b4a21d8407fe8babb5
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-04 14:06:57 -0600

    feat: configuración de babel para imports con alias

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['inline-dotenv'],
+  plugins: [
+    'inline-dotenv',
+    [
+      'module-resolver',
+      {
+        alias: {
+          components: './src/components',
+          constants: './src/constants',
+          hooks: './src/hooks',
+          navigations: './src/navigations',
+          screens: './src/screens',
+          services: './src/services',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
