7a45c1f0b052d6eeb5f3bf14bdaf60c04e2446ee
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-07 08:47:26 -0600

    feat: instalación de react native navigation

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import {SafeAreaView} from 'react-native';
 import {Provider as PaperProvider, Button} from 'react-native-paper';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <SafeAreaView
+      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={() => navigation.navigate('Notifications')}>
+        Go to notifications
+      </Button>
+    </SafeAreaView>
+  );
+}
+
+function NotificationsScreen({navigation}) {
+  return (
+    <SafeAreaView
+      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   console.log('hello debugger');
   return (
     <PaperProvider>
-      <SafeAreaView>
-        <Button
-          icon="camera"
-          mode="contained"
-          onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Notifications" component={NotificationsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
