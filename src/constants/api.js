143206dff9b59165aafe1af23eefcebc2b7516d2
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-09 22:54:30 -0600

    style: definir nuevas constantes version, lenguaje y path image de the movie db

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,5 @@
 export const TMDB_API_KEY = process.env.TMDB_API_KEY;
 export const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
+export const TMDB_VERSION = process.env.TMDB_VERSION;
+export const TMDB_LENGUAJE = process.env.TMDB_LENGUAJE;
+export const TMDB_PATH_IMAGE = process.env.TMDB_PATH_IMAGE;
