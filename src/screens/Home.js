91d0ea26dc671588e47788003f1ae666282d0e4d
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-10 20:52:01 -0600

    feat: agregar los generos de las películas en Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,31 @@
 import {ScrollView, View, StyleSheet} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {Title} from 'react-native-paper';
+import {Title, Text} from 'react-native-paper';
+import map from 'lodash/map';
 import {getNewsMovies} from 'services/movies/get-news-movies';
 import CarouselVertical from 'components/CarouselVertical';
 import MovieVerticalItem from 'components/movies/MovieVerticalItem';
 import {getMoviesWithGenres} from 'utils/get-movies-with-genres';
+import {getGenres} from 'services/movies/get-genres';
 
 export default function Home({navigation}) {
   const [newsMovies, setNewsMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [genreSelected, setGenreSelected] = useState(null);
+
+  const onChangeGenre = id => setGenreSelected(id);
 
   useEffect(() => {
     (async () => await moreNewsMovies(1))();
+  }, []);
+
+  useEffect(() => {
+    getGenres()
+      .then(data => {
+        setGenres(data);
+        onChangeGenre(data[0].id);
+      })
+      .catch(() => setGenres([]));
   }, []);
 
   const moreNewsMovies = async page => {
@@ -36,6 +51,29 @@ export default function Home({navigation}) {
           />
         </View>
       )}
+
+      <View style={styles.viewGenres}>
+        <Title style={styles.genresTitle}>Películas por genero</Title>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.genres}>
+          {map(genres, ({id, name}) => {
+            const styleColor = {
+              color: id !== genreSelected ? '#8697a5' : '#fff',
+            };
+
+            return (
+              <Text
+                key={id}
+                onPress={() => onChangeGenre(id)}
+                style={[styles.genre, styleColor]}>
+                {name}
+              </Text>
+            );
+          })}
+        </ScrollView>
+      </View>
     </ScrollView>
   );
 }
@@ -50,5 +88,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20,
     textAlign: 'center',
+  },
+  viewGenres: {
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  genres: {
+    marginTop: 5,
+    marginBottom: 15,
+    paddingHorizontal: 20,
+    padding: 10,
+  },
+  genresTitle: {
+    marginHorizontal: 20,
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  genre: {
+    marginRight: 20,
+    fontSize: 16,
   },
 });
