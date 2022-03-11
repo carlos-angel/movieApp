f01714f10b92f593f32ef8e2a4ade64f91d287f2
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-10 19:07:29 -0600

    feat: obtener los datos de una película en la screen Movie

--- src/screens/Movie.js
+++ src/screens/Movie.js
@@ -1,7 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {SafeAreaView, Text} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {getDetailMovie} from 'services/movies/get-detail-movie';
 
-export default function Movie() {
+export default function Movie({route, navigation}) {
+  const [movie, setMovie] = useState(null);
+  const {id, title} = route.params;
+
+  useEffect(() => {
+    const titleMovie = title.length > 24 ? `${title.slice(0, 24)}...` : title;
+    navigation.setOptions({title: titleMovie});
+  }, [title]);
+
+  useEffect(() => {
+    getDetailMovie(id)
+      .then(data => setMovie(data))
+      .catch(() => navigation.goBack());
+  }, [id]);
+
   return (
     <SafeAreaView
       style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
