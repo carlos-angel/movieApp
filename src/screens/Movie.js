ef7d551157521d5a962aebf1def4c08e61fe231e
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-11 21:27:05 -0600

    feat: mostrar los generos de la película en la screen Movie

--- src/screens/Movie.js
+++ src/screens/Movie.js
@@ -6,6 +6,7 @@ import {getDetailMovie} from 'services/movies/get-detail-movie';
 import endpoints from 'services/api';
 import Loading from 'components/common/Loading';
 import ModalVideo from 'components/common/ModalVideo';
+import map from 'lodash/map';
 
 export default function Movie({route, navigation}) {
   const [movie, setMovie] = useState(null);
@@ -22,7 +23,7 @@ export default function Movie({route, navigation}) {
     return <Loading message="cargando película" />;
   }
 
-  const {poster_path} = movie;
+  const {poster_path, title, genres} = movie;
   const poster_uri = endpoints.image.w500(poster_path);
 
   return (
@@ -44,6 +45,16 @@ export default function Movie({route, navigation}) {
             style={styles.play}
             onPress={() => setShowVideo(true)}
           />
+        </View>
+        <View style={styles.viewInformation}>
+          <Title>{title}</Title>
+          <View style={styles.genres}>
+            {map(genres, genre => (
+              <Text key={genre.id} style={styles.genre}>
+                {genre.name}
+              </Text>
+            ))}
+          </View>
         </View>
       </ScrollView>
       <ModalVideo show={showVideo} setShow={setShowVideo} idMovie={id} />
@@ -78,5 +89,15 @@ const styles = StyleSheet.create({
     height: 60,
     width: 60,
     borderRadius: 100,
+  },
+  viewInformation: {
+    marginHorizontal: 30,
+  },
+  genres: {
+    flexDirection: 'row',
+  },
+  genre: {
+    marginRight: 15,
+    color: '#8697a5',
   },
 });
